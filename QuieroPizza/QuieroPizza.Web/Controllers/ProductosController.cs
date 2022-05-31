d25a4a6e9cad586dbb3d43cfbd1445ffbc2f929f
Emilsson Soler <edennirs@gmail.com>
2022-05-31 10:37:17 -0600

    Creando lista de productos

--- QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -12,10 +12,23 @@ namespace QuieroPizza.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto = new ProductoModel();
-            producto.id = 1;
-            producto.Descripcion = "Pizza 6 queso";
-            return View(producto);
+            var producto1 = new ProductoModel();
+            producto1.id = 1;
+            producto1.Descripcion = "Pizza 6 queso";
+
+            var producto2 = new ProductoModel();
+            producto2.id = 2;
+            producto2.Descripcion = "Pizza Cuadrada";
+
+            var producto3 = new ProductoModel();
+            producto3.id = 3;
+            producto3.Descripcion = "Pizza Orilla de Queso";
+
+            var listaDeProducto = new List<ProductoModel>();
+            listaDeProducto.Add(producto1);
+            listaDeProducto.Add(producto2);
+            listaDeProducto.Add(producto3);
+            return View(listaDeProducto);
         }
     }
 }
